ff29dead697343b679c34ed2416d108549877b73
yeni <ngeetl@naver.com>
2024-02-02 22:48:05 +0900

    github pages 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const prefix =
+  process.env.NODE_ENV === 'production' ? 'https://ngeetl.github.io/yetion/' : ''
 
+const nextConfig = {
+  output: 'export',
+  assetPrefix: prefix,
+  }
 export default nextConfig;
--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <h1>레이아웃</h1>
       <body className={inter.className}>{children}</body>
     </html>
   );
