97f548b728f5f4f8bfd4d1190226d23a81337ab9
yeni <ngeetl@naver.com>
2024-02-04 19:24:59 +0900

    Intro, Navbar 반응형 디자인 설정

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,24 +6,23 @@ const Intro = () => {
   return (
     <div>
       {/* background image */}
-      <div id='bg' className='w-full mt-12'>
-        <div><img className="w-full" src="/img/profile_background.png" /></div>
+      <div id='bg' className='w-full md:h-[260px] h-[180px] mt-12 overflow-hidden'>
+        <div><img className="w-full h-full object-cover" src="/img/bg_4.jpg" /></div>
       </div>
 
       <div className='container relative'>
         {/* profile */}
-        <div id='profile' className='absolute -top-20'>
-          <div className='w-36 h-36 overflow-hidden rounded-3xl mb-2'>
+        <div id='profile' className='absolute -top-16 md:-top-20'>
+          <div className='md:w-36 md:h-36 w-28 h-28 overflow-hidden rounded-3xl mb-2'>
             <img className='w-full h-full object-cover' src='/img/profile_img.jpg'/>
           </div>
           <div className='font-bold text-xl'>YeinLee 이예인</div>
           <div className='text-sm font-medium'>능동적이고 진취적인 개발자</div>
         </div>
 
-
-        <div className='flex'>
+        <div className='flex md:flex-row flex-col'>
           {/* intro text */}
-          <div id='intro' className='pt-44 flex-1 w-full'>
+          <div id='intro' className='pt-44 flex-1 w-full '>
             <div className='text-2xl mb-2'>👩🏻‍💻💝</div>
             <div className='min-w-max leading-6'>
             안녕하세요. 코드와 친해지고 싶은 주니어 개발자 가다라혹 <br/>
@@ -37,12 +36,12 @@ const Intro = () => {
           </div>
 
           {/* contact list */}
-          <ul id='contact' className='flex-1 pt-20'>
-            <li className='bg-slate-100 rounded-xl px-10 py-6 min-w-max w-3/5 h-fit shadow-lg mx-auto'>📧 Email : ngeetl@naver.com</li>
-            <li className='bg-slate-100 rounded-xl px-10 py-6 min-w-max w-3/5 h-fit shadow-lg mt-5 mx-auto'>💻 Github : <Link href="https://github.com/ngeetl">github.com/ngeetl</Link></li>
-            <li className='bg-slate-100 rounded-xl px-10 py-6 min-w-max w-3/5 h-fit shadow-lg mt-5 mx-auto'>📗 Velog : <Link href="https://velog.io/@ngeetl">velog.io/@ngeetl</Link></li>
-            <li className='bg-slate-100 rounded-xl px-10 py-6 min-w-max w-3/5 h-fit shadow-lg mt-5 mx-auto'>📱 Phone : <Link href="tel: 010-7533-3855">010-7533-3855</Link></li>
-          </ul>
+          <ul id='contact' className='flex-1 md:pt-20 pt-8'>
+            <li className='bg-slate-100 rounded-xl px-10 py-6 md:min-w-max md:w-3/5 h-fit shadow-lg mx-auto'>📧 Email : ngeetl@naver.com</li>
+            <li className='bg-slate-100 rounded-xl px-10 py-6 md:min-w-max md:w-3/5 h-fit shadow-lg mt-5 mx-auto'>💻 Github : <Link href="https://github.com/ngeetl">github.com/ngeetl</Link></li>
+            <li className='bg-slate-100 rounded-xl px-10 py-6 md:min-w-max md:w-3/5 h-fit shadow-lg mt-5 mx-auto'>📗 Velog : <Link href="https://velog.io/@ngeetl">velog.io/@ngeetl</Link></li>
+            <li className='bg-slate-100 rounded-xl px-10 py-6 md:min-w-max md:w-3/5 h-fit shadow-lg mt-5 mx-auto'>📱 Phone : <Link href="tel: 010-7533-3855">010-7533-3855</Link></li>
+          </ul>`
         </div>
       </div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,19 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='h-12 px-4 flex justify-between items-center fixed top-0 left-0 bg-white w-full'>
+    <div className='h-12 px-4 flex justify-center md:justify-between items-center fixed top-0 left-0 bg-white w-full z-50'>
       {/* Logo img */}
       <Link href="/">
-      <div id='logo' className='w-9 h-9'>
+      <div id='logo' className='md:w-9 md:h-9 w-7 h-7'>
         <img src='/img/yetion_logo.jpg' className='object-cover' />
       </div>
       </Link>
       {/* Nav menu */}
+      <div className='md:hidden font-semibold ml-2'>
+        <Link href="/">이예인의 포트폴리오</Link>
+      </div>
       <div id='nav'>
-        <ul className='flex font-medium text-base'>
+        <ul className='md:flex hidden font-medium text-base'>
           <Link href="#Intro"><li className='rounded-lg py-2 px-4 mr-2 hover:bg-slate-100'>Profile</li></Link>
           <Link href="#Intro"><li className='rounded-lg py-2 px-4 mr-2 hover:bg-slate-100'>Stack</li></Link>
           <Link href="#Intro"><li className='rounded-lg py-2 px-4 mr-2 hover:bg-slate-100'>Carrer</li></Link>
